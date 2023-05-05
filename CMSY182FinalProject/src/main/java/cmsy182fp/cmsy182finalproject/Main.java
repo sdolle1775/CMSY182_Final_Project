0c1da5f8e963e49a4ee024d6066fcb469a88d222
sdolle1775 <125101358+sdolle1775@users.noreply.github.com>
2023-05-05 07:13:22 -0400

    Update Main.java

--- CMSY182FinalProject/src/main/java/cmsy182fp/cmsy182finalproject/Main.java
+++ CMSY182FinalProject/src/main/java/cmsy182fp/cmsy182finalproject/Main.java
@@ -1,10 +1,15 @@
+/**
+ * Bet Recorder - program to record and track bets
+ * CMSY167 Spring 2023
+ * @author Samuel Dolle
+ * @version 1.0
+ *
+ */
 package cmsy182fp.cmsy182finalproject;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,16 +22,12 @@ public class Main extends Application {
         stage.setTitle("Bet Recorder");
         stage.setScene(scene);
         stage.setResizable(false);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+        stage.show(); //load javafx scene
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        return fxmlLoader.load(); //load javafx fxml file
     }
     public static void main(String[] args) {
         launch();
